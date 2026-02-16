79c1552b3c4520372489ef63381b0a0106ea7bbe
Ahmad Mirza Rafiq Azmi <139027185+Amierza@users.noreply.github.com>
2026-02-16 11:29:16 +0700

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/login",
   plugins: [vue()],
   resolve: {
     alias: {
